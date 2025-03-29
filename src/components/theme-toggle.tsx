23503ee52e1cff594c3ec750186b34d7151a642f
Rizal Suryawan <rizalsuryawan86@gmail.com>
2025-03-29 22:55:35 +0700

    feat: enhance theme provider and toggle components with improved functionality and hydration handling

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,43 +1,29 @@
 "use client";
 
 import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
 import { Button } from "./ui/button";
-import { Moon, Sun } from "lucide-react";
+import { Moon, Sun, Monitor } from "lucide-react";
 
 export function ThemeToggle() {
-  const { theme, setTheme, systemTheme } = useTheme();
-  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
 
-  // Fix hydration issue
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    return (
-      <Button
-        variant="ghost"
-        size="icon"
-        className="rounded-full"
-        aria-label="Toggle theme"
-        disabled
-      />
-    );
-  }
-
-  const currentTheme = theme === "system" ? systemTheme : theme;
+  const nextTheme = () => {
+    if (theme === "system") return "light";
+    if (theme === "light") return "dark";
+    return "system";
+  };
 
   return (
     <Button
       variant="ghost"
       size="icon"
-      onClick={() => setTheme(currentTheme === "dark" ? "light" : "dark")}
-      className="rounded-full "
+      onClick={() => setTheme(nextTheme())}
+      className="rounded-full cursor-pointer"
       aria-label="Toggle theme"
     >
-      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 duration-500" />
-      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 duration-500" />
+      <Sun className="h-[1.2rem] w-[1.2rem] absolute rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 system:scale-0 duration-500" />
+      <Moon className="h-[1.2rem] w-[1.2rem] absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 system:scale-0 duration-500" />
+      <Monitor className="h-[1.2rem] w-[1.2rem] absolute scale-0 transition-all system:scale-100 dark:scale-0 duration-500" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
