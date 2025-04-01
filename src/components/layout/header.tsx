4215d2ed2c76fb4c3789b06ec1a66a4a7635acad
Rizal Suryawan <rizalsuryawan86@gmail.com>
2025-04-01 14:19:52 +0700

    fix: adjust z-index for header component

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -2,7 +2,7 @@ import Navbar from "./navbar";
 
 export default function Header() {
   return (
-    <header className="sticky top-0 w-full">
+    <header className="sticky top-0 z-50 w-full">
       <Navbar />
     </header>
   );
