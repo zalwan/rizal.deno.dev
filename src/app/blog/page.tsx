304e4bd67ec7a5ca78d16e1a45f5e06a461ecb75
Rizal Suryawan <rizalsuryawan86@gmail.com>
2025-04-04 01:16:46 +0700

    feat: implement drawer component in blog page for enhanced user interaction

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,56 @@
-export default async function Page() {
+"use client";
+
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+
+export default function Page() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="wrapper px-4 py-3">
-      <h1>Hello from blog!</h1>
+    <div
+      className={`origin-center transform transition-transform duration-300 ease-in-out ${
+        isOpen ? "scale-90" : "scale-100"
+      }`}
+    >
+      <div className="wrapper flex flex-col items-center justify-center px-4 py-3">
+        <p className="mb-10">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt
+          consectetur aliquid reiciendis deserunt harum repellat provident
+          voluptatem quod earum, nesciunt dolore dolorum culpa quam laborum
+          ducimus veniam facere labore exercitationem?
+        </p>
+        <Drawer onOpenChange={setIsOpen}>
+          <DrawerTrigger asChild>
+            <Button variant="outline">Open Drawer</Button>
+          </DrawerTrigger>
+          <DrawerContent>
+            <DrawerHeader>
+              <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+              <DrawerDescription>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic
+                doloribus ipsam rem deserunt vitae corporis repellendus
+                quibusdam, ut at autem ipsum, incidunt neque cum dolores
+                laudantium, ullam est. Natus, eligendi.
+              </DrawerDescription>
+            </DrawerHeader>
+            <DrawerFooter>
+              <DrawerClose asChild>
+                <Button variant="outline">Cancel</Button>
+              </DrawerClose>
+            </DrawerFooter>
+          </DrawerContent>
+        </Drawer>
+      </div>
     </div>
   );
 }
