5dc1046263f20ebc24c8e4a54f3433afceabed3f
Rizal Suryawan <rizalsuryawan86@gmail.com>
2025-04-07 23:28:20 +0700

    fix: update asset type definition in webpack configuration for clarity

--- next.config.ts
+++ next.config.ts
@@ -16,7 +16,7 @@ const nextConfig: NextConfig = {
   webpack: (config) => {
     config.module.rules.push({
       test: /\.(png|jpg|jpeg|gif|svg|webp)$/i,
-      type: "asset/resource", // File akan di-output sebagai aset terpisah
+      type: "asset/resource",
       generator: {
         filename: "static/media/[name].[hash][ext]",
       },
